62d7e8bca09d4ec6c79fd0d67672c543959abeea
Jeffrey Fuller <jjafuller@gmail.com>
2016-12-04 12:08:26 -0500

    refactored the walk method to just build a list, and implement proof of concept guid replacement

--- command/dotnet.go
+++ command/dotnet.go
@@ -9,14 +9,18 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
+	"sort"
 	"strings"
 
 	"github.com/jjafuller/ouroboros/command/dotnet"
+	"github.com/satori/go.uuid"
 )
 
+// in solution
 // ^Project.*{(.*)}"$
 // Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Test", "Test\Test.csproj", "{8EA60CA5-7D3D-4813-ACB1-069618285452}"
 
+// in project
 // ^\s*<ProjectGuid>{(.*)}</ProjectGuid>\s*$
 //     <ProjectGuid>{8EA60CA5-7D3D-4813-ACB1-069618285452}</ProjectGuid>
 
@@ -24,7 +28,6 @@ var (
 	tplPath, tplName, dstPath, dstName string
 	ignore                             = regexp.MustCompile(`\.vs|\.git|\.DS_Store|Thumbs.db|bin/|obj/|packages/`)
 	guidInSln                          = regexp.MustCompile(`^Project.*{(?P<guid>.*)}"$`)
-	guidInProj                         = regexp.MustCompile(`^\s*<ProjectGuid>{(?P<guid>.*)}</ProjectGuid>\s*$`)
 )
 
 // DotnetCommand creates a .NET solution from a .NET solution
@@ -39,84 +42,179 @@ func (c *DotnetCommand) Run(args []string) int {
 	cmdFlags := flag.NewFlagSet("dotnet", flag.ContinueOnError)
 	cmdFlags.Usage = func() { c.Ui.Output(c.Help()) }
 
-	//newGuid := flag.Bool("new-guid", false, "")
+	newGUIDs := cmdFlags.Bool("new-guids", false, "generate new guids?")
 
 	if err := cmdFlags.Parse(args); err != nil {
 		c.Ui.Error(fmt.Sprintf("Invalid arguments: %s\n", err))
 		return 1
 	}
 
-	if len(args) != 2 {
+	if len(cmdFlags.Args()) != 2 {
 		c.Ui.Error("The dotnet command expects two arguments\n")
 		return 1
 	}
 
-	tplPath = args[0]
-	dstPath = args[1]
+	tplPath = cmdFlags.Arg(0)
+	dstPath = cmdFlags.Arg(1)
 
 	tplName = path.Base(tplPath)
 	dstName = path.Base(dstPath)
 
-	fmt.Printf("Template: %s\n", tplName)
-	fmt.Printf("Destination: %s\n", dstName)
+	fmt.Printf("Template: \n  %s\n", tplName)
+	fmt.Printf("Destination: \n  %s\n", dstName)
 
 	// ensure destination directory
 	if _, err := os.Stat(dstPath); os.IsNotExist(err) {
 		os.Mkdir(dstPath, 0755)
 	}
 
-	err := filepath.Walk(tplPath, visit)
-	fmt.Printf("filepath.Walk() returned %v\n", err)
+	dirList, slnPath, err := getDirectoryList(tplPath)
+
+	if err != nil {
+		fmt.Printf("getDirectoryList() returned %v\n", err)
+		return 1
+	}
+
+	if len(slnPath) < 1 {
+		fmt.Println("No solution file was found within the directory")
+		return 1
+	}
+
+	fmt.Printf("Solution file: \n  %s\n", slnPath)
+
+	var guids map[string]string
+
+	if *newGUIDs {
+		fmt.Println("Generating new guids...")
+
+		guidList, err := c.extractGUIDsFromSln(slnPath)
+
+		if err != nil {
+			fmt.Printf("Failed to extract guids from the solution file: %v\n", err)
+			return 1
+		}
+
+		guids = c.GeneratenewGUIDs(guidList)
+
+		for k, v := range guids {
+			fmt.Printf("  %s -> %s\n", k, v)
+		}
+	}
+
+	err = copyItems(dirList, guids)
+
+	if err != nil {
+		fmt.Printf("Failed generate new items: %v\n", err)
+		return 1
+	}
 
 	return 0
 }
 
-func visit(filePath string, fi os.FileInfo, err error) error {
-	rel := strings.Replace(filePath, tplPath, "", 1)
+func getDirectoryList(dirPath string) (map[string]os.FileInfo, string, error) {
+	dirList := make(map[string]os.FileInfo)
+	slnPath := ""
 
-	if len(rel) == 0 || ignore.MatchString(rel) {
+	err := filepath.Walk(dirPath, func(filePath string, fi os.FileInfo, walkErr error) error {
+		// if an error occurred while walking the directory bubble up
+		if walkErr != nil {
+			return walkErr
+		}
+
+		// get the file path relative to the dir path
+		rel := strings.Replace(filePath, dirPath, "", 1)
+
+		// if we are at the root, or are looking at an ignored file skip it
+		if len(rel) == 0 || ignore.MatchString(rel) {
+			return nil
+		}
+
+		ext := strings.ToLower(filepath.Ext(fi.Name()))
+
+		if ext == ".sln" {
+			slnPath = filePath
+		}
+
+		dirList[rel] = fi
+
 		return nil
-	}
+	})
 
-	dstRel := strings.Replace(rel, tplName, dstName, -1)
-
-	tpl := filePath
-	dst := path.Join(dstPath, dstRel)
-
-	copyErr := copy(tpl, dst, fi)
-	if copyErr != nil {
-		fmt.Printf("Error: %s\n", copyErr)
-	}
-
-	fmt.Printf("Visited: %s\n", rel)
-	return nil
+	return dirList, slnPath, err
 }
 
-// ExtractGUID extracts guids from file contents
-func (c *DotnetCommand) ExtractGUID(contents, ext string) (err error) {
-	lines := strings.Split(contents, "\n")
-
-	guids := []string{}
-
-	re := guidInSln
-	if ext != ".sln" {
-		re = guidInProj
+func (c *DotnetCommand) extractGUIDsFromSln(filePath string) (guids []string, err error) {
+	contents, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return
 	}
 
+	guids, err = c.ExtractGUIDsFromString(string(contents))
+
+	return
+}
+
+// ExtractGUIDsFromString extracts guids from file contents
+func (c *DotnetCommand) ExtractGUIDsFromString(contents string) (guids []string, err error) {
+	lines := strings.Split(contents, "\n")
+
+	guids = []string{}
+
 	for _, line := range lines {
-		matches := re.FindStringSubmatch(line)
+		matches := guidInSln.FindStringSubmatch(strings.TrimSpace(line))
 
 		if len(matches) > 1 {
 			guids = append(guids, matches[1])
 		}
 	}
 
-	fmt.Printf("%v\n", guids)
+	return
+}
+
+// GeneratenewGUIDs takes a list of GUIDs and generates a new GUID to replaced
+// each GUID in the list
+func (c *DotnetCommand) GeneratenewGUIDs(guids []string) (guidMap map[string]string) {
+	guidMap = make(map[string]string)
+
+	for _, guid := range guids {
+		guidMap[guid] = strings.ToUpper(uuid.NewV4().String())
+	}
 
 	return
 }
 
-func copy(src, dst string, fi os.FileInfo) (err error) {
+func copyItems(dirList map[string]os.FileInfo, guids map[string]string) (err error) {
+	fmt.Println("Generating new items...")
+
+	// first we need to get our list of paths in order
+	paths := []string{}
+
+	for relPath := range dirList {
+		paths = append(paths, relPath)
+	}
+
+	sort.Strings(paths)
+
+	// now that we have the paths in order we can iteration and copy/transform the items
+	for _, relPath := range paths {
+		// transform the path to use the new solution name
+		dstRel := strings.Replace(relPath, tplName, dstName, -1)
+
+		tpl := path.Join(tplPath, relPath)
+		dst := path.Join(dstPath, dstRel)
+
+		err = copyItem(tpl, dst, dirList[relPath], guids)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("  %s -> %s\n", relPath, dstRel)
+	}
+
+	return err
+}
+
+func copyItem(src, dst string, fi os.FileInfo, guids map[string]string) (err error) {
 	if fi.IsDir() {
 		if _, err := os.Stat(dst); os.IsNotExist(err) {
 			os.Mkdir(dst, 0755)
@@ -127,9 +225,9 @@ func copy(src, dst string, fi os.FileInfo) (err error) {
 		// if this file is a type known to contain project names,
 		// edit it, otherwise just copy it
 		if _, ok := dotnet.VsData.SourceFileExts[ext]; ok {
-			err = copyFileAndEdit(src, dst)
+			err = transformFile(src, dst, guids)
 		} else if _, ok := dotnet.VsData.ProjectFileExts[ext]; ok {
-			err = copyFileAndEdit(src, dst)
+			err = transformFile(src, dst, guids)
 		} else {
 			err = copyFile(src, dst)
 		}
@@ -138,15 +236,23 @@ func copy(src, dst string, fi os.FileInfo) (err error) {
 	return
 }
 
-func copyFileAndEdit(src, dst string) (err error) {
+func transformFile(src, dst string, guids map[string]string) (err error) {
 	read, err := ioutil.ReadFile(src)
 	if err != nil {
 		return
 	}
 
+	// rename
 	newContents := strings.Replace(string(read), tplName, dstName, -1)
 
-	err = ioutil.WriteFile(dst, []byte(newContents), 0)
+	// replace guids
+	if guids != nil {
+		for k, v := range guids {
+			newContents = strings.Replace(newContents, k, v, -1)
+		}
+	}
+
+	err = ioutil.WriteFile(dst, []byte(newContents), 0666)
 
 	return
 }
@@ -189,7 +295,8 @@ func (c *DotnetCommand) Synopsis() string {
 // Help provides detailed usage information for this command
 func (c *DotnetCommand) Help() string {
 	helpText := `
-
+USAGE: ouroboros dotnet <args> [tpl path] [dst path]
+  new-guids      Generate new project GUIDs
 `
 	return strings.TrimSpace(helpText)
 }
